84d11be5468a72cf90b526046be5978901f57bcd
NafisaMehreen <100036933+NafisaMehreen@users.noreply.github.com>
2023-12-01 02:44:33 +0600

    notif change

---
+++ frontend/src/lib/apiList.js
@@ -0,0 +1,14 @@
+export const server = "http://localhost:4444";
+
+const apiList = {
+  login: `${server}/auth/login`,
+  signup: `${server}/auth/signup`,
+  uploadResume: `${server}/upload/resume`,
+  uploadProfileImage: `${server}/upload/profile`,
+  jobs: `${server}/api/jobs`,
+  applications: `${server}/api/applications`,
+  user: `${server}/api/user`,
+  applicants: `${server}/api/applicants`,
+};
+
+export default apiList;
