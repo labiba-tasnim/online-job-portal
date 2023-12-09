dc7107f5ff6551934595550cd899a8fa6f1daaca
sanjanatroyee <149012699+sanjanatroyee@users.noreply.github.com>
2023-12-09 10:18:21 +0600

    Update Navbar.js

--- frontend/src/component/Navbar.js
+++ frontend/src/component/Navbar.js
@@ -56,9 +56,6 @@ const Navbar = (props) => {
               <Button color="inherit" onClick={() => handleClick("/employees")}>
                 Employees
               </Button>
-              <Button color="inherit" onClick={() => handleClick("/chat")}>
-                Inbox
-              </Button>
               <Button color="inherit" onClick={() => handleClick("/connections")}>
                 Connections
               </Button>
@@ -82,9 +79,6 @@ const Navbar = (props) => {
                 onClick={() => handleClick("/applications")}
               >
                 Applications
-              </Button>
-              <Button color="inherit" onClick={() => handleClick("/chat")}>
-                Inbox
               </Button>
               <Button color="inherit" onClick={() => handleClick("/connections")}>
                 Connections
